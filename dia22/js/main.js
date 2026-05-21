b8831a617a36101d1a6c4e0c15cb05e19572acfb
mariannelis <lucena.lis14@gmail.com>
2026-05-21 18:58:11 +0200

    dia

---
+++ dia22/js/main.js
@@ -0,0 +1,12 @@
+// ejercicio 1//
+const empty = document.querySelector('#empty');
+empty.textContent = '¡Hola Mundo!';
+
+// ejercicio 2//    
+const color = document.querySelector('#color');
+color.style.color = 'hsla(133, 66%, 52%, 0.50)';
+color.textContent = '¡Ahora soy amarillo!';
+// ejercicio 1-3//
+const size = document.querySelector('#size');
+size.style.fontSize = '24px';
+size.textContent = '¡Ahora soy más grande!';
