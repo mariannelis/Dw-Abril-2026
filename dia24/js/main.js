85a79287a91204f7081bbcacd1c15fabd92fc4ec
mariannelis <lucena.lis14@gmail.com>
2026-05-28 03:45:31 +0200

    dia

--- dia24/js/main.js
+++ dia24/js/main.js
@@ -1,7 +1,22 @@
-
-let boton = document.getElementById("boton");
+//Nivel 2
+let boton = document.getElementById("botonMostrar");
 boton.addEventListener("click", function() { 
     let contenido = document.getElementById("texto").value;
     document.getElementById("resultado").textContent = contenido;
+});
+
+//nivel 3 -Calculadora
+let boton2 = document.getElementById("botonSumar");
+boton2.addEventListener("click", function() {
+
+    let numero1 = Number(document.getElementById("num1").value);
+
+    let numero2 = Number(document.getElementById("num2").value);
+
+    let suma = numero1 + numero2;
+
+    document.getElementById("resultado2").textContent =
+        "Resultado: " + suma;
+
 });
 
