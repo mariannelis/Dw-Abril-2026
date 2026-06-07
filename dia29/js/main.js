f172ca7240b7dec752f78aa9aa4d23d532341029
mariannelis <lucena.lis14@gmail.com>
2026-06-07 23:38:17 +0200

    dia

--- dia29/js/main.js
+++ dia29/js/main.js
@@ -1,6 +1,6 @@
 // nivel 1 -Funciones básicas con consola
 function mostrarPares() {
-    for (let i = 2; i <= 100; i += 2) {
+    for (let i = 1; i <= 100; i += 1) {
         console.log(i);
     }
 }
@@ -16,6 +16,7 @@ function cuadrado(x) {
         console.log(text);
     }
 }
+
 cuadrado(5);
 console.log('');
 function cuadradoHueco(x) {
@@ -61,3 +62,46 @@ function triangulo(x) {
 }
 triangulo(5);
 
+function saludar(nombre) {
+    alert(`Hola ${nombre}`);
+    console.log(`Hola ${nombre}`);
+}
+
+function procesarEntradaUsuario(callback) {
+    const nombre = prompt('Por favor ingresa tu nombre:');
+    callback(nombre);
+}
+procesarEntradaUsuario(saludar);
+
+// Nivel 3:  Temporizadores
+// mostrar un mensaje después de 3 segundos
+setTimeout(() => {
+    console.log('¡Hola! Este mensaje se muestra después de 3 segundos.');
+}, 3000);
+// mostrar la hora actual cada segundo
+setInterval(() => {
+    const ahora = new Date();
+    console.log(`La hora actual es: ${ahora.toLocaleTimeString()}`);
+}, 1000);
+
+// Contaddor con setInterval
+let contador = 0;
+const intervalo = setInterval(() => {
+    const contadorDOM = document.getElementById('contador');
+    const botonDisminuir = document.getElementById('disminuir');
+    const botonReiniciar = document.getElementById('reiniciar');
+    const botonAumentar = document.getElementById('aumentar');
+    botonAumentar.addEventListener('click', () => {
+    valorContador++; // Suma 1
+    contadorDOM.textContent = valorContador;
+});
+
+botonDisminuir.addEventListener('click', () => {
+    valorContador--; // Resta 1
+    contadorDOM.textContent = valorContador;
+});
+
+botonReiniciar.addEventListener('click', () => {
+    valorContador = 0; // Reinicia a 0
+    contadorDOM.textContent = valorContador;
+});
